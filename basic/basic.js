1508ae3a49a3c9f034dc478dc6954c4323454806
Heesu974 <heesu.kang95@gmail.com>
2021-07-04 15:28:58 +0900

    update basic

---
+++ basic/basic.js
@@ -0,0 +1,69 @@
+// const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+// //get item form array
+// console.log(daysOfWeek[5])
+
+// //add one more day to the array
+// console.log(daysOfWeek)
+// daysOfWeek.push('sun');
+// console.log(daysOfWeek);
+
+// const player = ['nico', 1212, false, 'little bit'];
+
+// const player = {
+//     name: 'nico',
+//     points: 10,
+//     fat: true,
+// };
+// console.log(player);
+// player.fat = false;
+// console.log(player);
+// player.lastName = 'potato';
+// console.log(player);
+// player.points = player.points + 15;
+
+// function sayHello(nameOfPerson, age) {
+//     console.log('hello my name is' + nameOfPerson + 'and my age is ' + age)
+// }
+// sayHello('nico', 10);
+// sayHello('maya', 8);
+// sayHello('heesu', 12);
+
+// function plus(a, b) {
+//     console.log(a + b);
+// }
+// function divide(a, b) {
+//     console.log(a / b);
+// }
+// plus(8, 60);
+// divide(60, 3)
+
+// const player = {
+//     name: 'nico',
+//     sayHello: function (otherPersonName) {
+//         console.log('hello ' + otherPersonName + ' nice to meet you');
+//     }
+
+// }
+// player.sayHello('maya');
+
+// function plus(a, b) {
+//     console.log(a + b);
+// }
+// plus(5, 5)
+
+// const calculator = {
+//     plus: function (a, b) {
+//         return a + b;
+//     },
+//     divide: function (a, b) {
+//         return a / b;
+//     },
+//     times: function (a, b) {
+//         return a * b;
+//     },
+
+// }
+// const plusResult = calculator.plus(10, 6);
+// const divideResult = calculator.divide(plusResult, 4)
+
